bc1789428daaafe861d7ee765fa7813957fccfb4
PabloMendes17 <firefoxpablo@gmail.com>
2024-02-01 06:02:25 -0300

    Inicio CSS

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,6 +3,8 @@
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
+/*importando o framework bootstrap*/
+import 'bootstrap';
 
 import axios from 'axios';
 window.axios = axios;
