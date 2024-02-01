704ec4d29ffece1750f706a389a024a3d02c37c6
PabloMendes17 <firefoxpablo@gmail.com>
2024-02-01 17:06:31 -0300

    BootFuncionando

--- resources/views/viewpaginaPrincipal.blade.php
+++ resources/views/viewpaginaPrincipal.blade.php
@@ -8,10 +8,37 @@
     
 </head>
 <body>
-<h1>Ola! {{$user}}</h1>
-    <a href='/vitrine'>Clique</a>
-    <a href='/info'>Informações</a><br><br>
-    <a href='/cadastrar'>Cadastrar Produtos</a>
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-bottom: 5rem;">
+        <div class="container-fluid"> 
+            <img class="logoNav" src="./SRC/images/logogalago.png"/>
+            <div class="logado" >usuario@galago.com.br <br/>sex 23 de Jan 2024 11:40</div>
+        </div>
+    </nav> 
+    <div class="container-fluid" style="display: flex; justify-content: center;">
+        <div class="card" style="width: 18rem; margin:1rem;">
+            <img src="C:\Users\flags\Desktop\estudo\ProjetoCRMGalago\SRC\images\AgendaSVG.SVG" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">AGENDA</h5>
+              <p class="card-text">Registre e consulte aqui demonstrações e retornos.</p>
+              <a href="#" class="btn btn-primary btn-block">Agendamentos</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem; margin:1rem;">
+            <img src="C:\Users\flags\Desktop\estudo\ProjetoCRMGalago\SRC\images\AtendimentoSVG.SVG" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">ATENDIMENTO</h5>
+              <p class="card-text">Registre e consulte aqui os atendimentos.</p>
+              <a href="#" class="btn btn-primary">Atendimentos</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem; margin:1rem;">
+            <img src="C:\Users\flags\Desktop\estudo\ProjetoCRMGalago\SRC\images\TreinamentoSVG.SVG" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">TREINAEMNTO</h5>
+              <p class="card-text">Registre e consulte aqui os treinamentos.</p>
+              <a href="#" class="btn btn-primary">Treinamentos</a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
