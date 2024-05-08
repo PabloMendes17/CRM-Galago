d831215891b8e50a2d127fd67643ea0a622ae251
PabloMendes17 <firefoxpablo@gmail.com>
2024-05-08 07:52:16 -0300

    Tratamento Servidor fora

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\Vendedor;
+use Exception;
 
 
 class LoginController extends Controller{
@@ -27,24 +28,35 @@ class LoginController extends Controller{
             'password' => ['required'],
         ]);
 
-        $user = Vendedor::where('email', $request->email)->first();
+       try{
 
-        if(!$user){
+            $user = Vendedor::where('email', $request->email)->first();
+
+            if(!$user){
+                return back()->withErrors([
+                    'email' => 'Usuário Inválido.',
+                ]);
+
+            }elseif(Auth::guard('vendedor')->attempt($credentials,$remember = true)) {
+
+                    $request->session()->regenerate();
+
+                    return redirect()->intended('PaginaPrincipal');
+            }
+
+            
             return back()->withErrors([
-                'email' => 'Usuário Inválido.',
+                'email' => 'Senha Inválida.',
+            ])->withInput($request->only('email'));
+
+       }catch (Exception $e){
+
+            return back()->withErrors([
+                'email' => 'Servidor Inoperante entre em contato com suporte.',
             ]);
 
-        }elseif(Auth::guard('vendedor')->attempt($credentials,$remember = true)) {
-
-                $request->session()->regenerate();
-
-                return redirect()->intended('PaginaPrincipal');
-        }
-
-        return back()->withErrors([
-            'email' => 'Senha Inválida.',
-        ])->withInput($request->only('email'));
-        
+       }
+      
 
     }
 
