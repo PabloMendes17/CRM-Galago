78359a7e457280f8ee82d08ebc4d7cec6560c2fb
PabloMendes17 <firefoxpablo@gmail.com>
2024-07-24 08:13:47 -0300

    AlteraControllerLogin

--- app/Http/Controllers/SitePublicoController.php
+++ app/Http/Controllers/SitePublicoController.php
@@ -12,7 +12,7 @@ use Exception;
 use App\Models\agenda;
 use App\Models\cliente;
 use App\Models\situacao_agenda;
-use App\Models\vendedor;
+use App\Models\Vendedor;
 
 
 class SitePublicoController extends Controller
